fe7ad9525248d3b675e1dfa0bc6bea07d53c50d4
Adriana Thanner <thanneradriana@hotmail.com>
2023-03-09 13:15:17 -0300

    Desafío 2

--- desafio2.js
+++ desafio2.js
@@ -1,16 +1,13 @@
+
 const fs = require ("fs");
 
 class ProductManager {
     #NextID = 0;
-    #path;
-
-    constructor (path) {
-    this.#path = path;
-}
+    path = "./Products.json";
 
 async getProducts() {
     try {
-        const productos = await fs.promises.readfile(this.#path, "utf-8")
+        const productos = await fs.promises.readFile(this.path, "utf-8")
         return JSON.parse(productos)
     }catch (e){
       return [];
@@ -20,7 +17,7 @@ async getProducts() {
     
 async addProduct (title, description, price, thumbnail, code, stock) {
     let id = this.#NextID += 1;
-
+   
     const product ={
 
     id: id,
@@ -38,42 +35,58 @@ async addProduct (title, description, price, thumbnail, code, stock) {
     stock: [10]
      
     }
-
-    // const updateProduct = [...product, newProduct];
-   await fs.promises.appendFile(this.#path, JSON.stringify(product));
+    
+    const allProduct = await this.getProducts()
+    allProduct.push(product)
+   return await fs.promises.writeFile(this.path, JSON.stringify(allProduct));
 }
 
 async getProductsById (getbyId){
-    const products = await this.getProducts();
-    const upDateP = await products.find((p) => p.getbyId === getbyId)
     try{
-        console.log(upDateP)
-    } catch (e) {
-        throw new Error (`No se puede actualizar`);
+        const products = await this.getProducts();
+        const upDateP = await products.find((p) => p.id === getbyId)
+        return upDateP;
+    } catch (e){
+        return [];
     }
     }
+// 
+async upDateProductId (upId, dataToUpdate){
+    
+        const products = await this.getProducts();
+        let aux = products.filter((e)=> e.id !== upId)
 
-// async updateProduct (getbyId, upDate){
-//     const products = await this.getProducts();
-//     const upDateP = products.find((p) => p.getbyId === getbyId)
+        let upDateProduct = products.find((p) => p.id === upId)
+        if(dataToUpdate.hasOwnProperty("title") || dataToUpdate.hasOwnProperty("description") || dataToUpdate.hasOwnProperty("price")){
+          upDateProduct.title = dataToUpdate.title ? dataToUpdate.title : upDateProduct.title
+          upDateProduct.description = dataToUpdate.description ? dataToUpdate.description : upDateProduct.description
+          upDateProduct.price = dataToUpdate.price ? dataToUpdate.price : upDateProduct.price
+          aux.push(upDateProduct)
+           return await fs.promises.writeFile(this.path, JSON.stringify(aux))
+        }
 
-//     if(!upDateP){
-//         await fs.promises.writeFile(this.#path,"utf-8",JSON.stringify(upDate))
-//     } else {
-//         throw new Error (`No se puede actualizar`);
-//     }
-// }
+        } 
 
-// async deletProduct (){
-
-// }
-
+async deletProduct (deletbyId){
+        try{
+            const products = await this.getProducts();
+           const deletedProduct = await products.filter((p) => p.id !== deletbyId)
+           await fs.promises.writeFile(this.path, JSON.stringify(deletedProduct))
+           return deletedProduct
+        } catch (e){
+            throw new Error (e.message);
+        }
+        }
 }
 
 async function main (){
-    const manager = new ProductManager("./Products.json");
-    manager.addProduct ("Escada", "Especially Elixir EDP 30 Ml", 90, "Imagen", "0023")
-    manager.addProduct ("Esca", "Espec0 Ml", 70, "Imagen", "0033")
-    manager.getProductsById(1)
+    const manager = new ProductManager();
+    await manager.addProduct("Escada", "Especially Elixir EDP 30 Ml", 90, "Imagen", "0023")
+    await manager.addProduct ("Esca", "Espec0 Ml", 70, "Imagen", "0033")
+    await manager.upDateProductId(1,{ description: "lola la vaca", price: 50})
+    console.log (await manager.getProductsById(1))
+    console.log (await manager.getProductsById(2))
+    console.log (await manager.deletProduct(2))
 }
+
 main();
